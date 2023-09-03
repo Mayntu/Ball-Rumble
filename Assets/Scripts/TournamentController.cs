34cfd2fbddf6ef6aa51f8300c549b682885a7b34
vitnemikin <vit.nemikin@yandex.ru>
2023-09-03 17:07:47 +0600

    Сделано динамическое определение количества юнитов в команде. Тем не менее, оно должно быть одинаковым, иначе NullReferenceException неизбежен!

--- Assets/Scripts/TournamentController.cs
+++ Assets/Scripts/TournamentController.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class TournamentController : MonoBehaviour {
     [SerializeField] private uint totalTeams = 2;
-    [SerializeField] private uint unitsInTeam = 1; // 6 units should be in release
+    [SerializeField] private uint unitsInTeam = 6; // в релизной версии должно быть по 6 юнитов в команде
+    /* Важно! В любом случае, в каждой команде должно быть одинаковое количество юнитов!
+       Иначе игра упадёт с NullReferenceException */
     [SerializeField] private uint updatesPerRequest = 3;
     private uint updatesCount = 0;
     private TournamentPlayer[] teams = null;
@@ -34,19 +36,19 @@ public class TournamentController : MonoBehaviour {
     }
 
     private void createObjectsInfo() {
-        // ����� ������� objectsInfo: ���������� ������ � ������ ������� + ��� + 4 ������ �����
+        // размер массива в objectsInfo: количество команд * юнитов в команде + мяч + 4 штанги
         objectsInfo.data = new UnitInfo[totalTeams * unitsInTeam + 1 + 4];
         Debug.Log(objectsInfo.data.Length);
-        // ��������� ���
+        // добавляем мяч
         objectsInfo.data[0] = new UnitInfo(GameObject.FindWithTag("Ball"));
         uint i = 1;
-        // ��������� ������
+        // добавляем штанги
         GameObject[] objects = GameObject.FindGameObjectsWithTag("GoalPost");
         foreach (GameObject obj in objects) {
             objectsInfo.data[i] = new UnitInfo(obj);
             i++;
         }
-        // ��������� ����� ������
+        // добавляем юниты
         for (uint t=0; t<totalTeams; t++) {
             objects = GameObject.FindGameObjectsWithTag(teams[t].playerName());
             foreach(GameObject obj in objects) {
@@ -64,6 +66,7 @@ public class TournamentController : MonoBehaviour {
             int playerPort = 8201 + i;
             TeamInfo client = new(i, playerName, playerHost, playerPort);
             teams[i] = new TournamentPlayer(client);
+            unitsInTeam = (uint) GameObject.FindGameObjectsWithTag(playerName).Length;
         }
     }
 }
