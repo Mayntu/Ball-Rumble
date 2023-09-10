91ad9c3d4c11521118dd198c45468234d06a4ba5
Munity2325 <konradinikita2000@gmail.com>
2023-09-10 14:40:56 +0600

    skin activation

--- Assets/Scripts/PlayerType.cs
+++ Assets/Scripts/PlayerType.cs
@@ -6,14 +6,27 @@ public class PlayerType : MonoBehaviour
 {
     public bool isRed, isBlue, isLightBlue, isYellow, isPurple, isBlack, isWhite, isPink;
 
+    public Animator animator;
+    public Avatar blackAvatar, yellowAvatar;
+    public GameObject blackSkin, yellowSkin;
+
     private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        if (isBlack)
+        {
+            animator.avatar = blackAvatar;
+            blackSkin.SetActive(true);
+        }
+        else if (isYellow)
+        {
+            animator.avatar = yellowAvatar;
+            yellowSkin.SetActive(true);
+        }
     }
 
     private void Update()
     {
-        
     }
     
     public void ResetColor()
