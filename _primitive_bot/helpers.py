1bf3903085963cf875016b9c2bfc808d510772b4
vitnemikin <vit.nemikin@yandex.ru>
2023-09-23 18:57:42 +0600

    direction правильно считается
    Функция helpers.direction() исправлена, теперь юниты бегут к мячу в 99%
    
    Добавлена функция helpers.distance()

--- _primitive_bot/helpers.py
+++ _primitive_bot/helpers.py
@@ -1,11 +1,19 @@
 import math
 
 
+# Возвращает направление в градусах от точки point1 к точке point2
 def direction(point1, point2):
+    dx = point2['x'] - point1['x']
+    dz = point2['z'] - point1['z']
     try:
-        tg = (point2['z'] - point1['z']) / (point2['x'] - point1['x'])
-        a = math.atan(tg)
+        a = math.atan2(dz, dx)
     except ZeroDivisionError:
-        return 0
-    return a * 180 / math.pi
+        return 90 if dz > 0 else -90
+    return math.degrees(a)
 
+
+# Возвращает расстояние от точки point1 до точки point2
+def distance(point1, point2):
+    dx = point2['x'] - point1['x']
+    dz = point2['z'] - point1['z']
+    return math.sqrt(dx**2 + dz**2)
